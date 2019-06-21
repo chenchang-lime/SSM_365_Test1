2fae13f09daf2b9af9c5cdbf7ba5b169e31b35df
chenchang <294689504@qq.com>
2019-06-21 17:18:38 +0800

    项目初始化

---
+++ src/com/tianmao/chenchang/chenchang.java
@@ -0,0 +1,9 @@
+package com.tianmao.chenchang;
+
+public class chenchang {
+
+	public static void main(String[] args) {
+		System.out.println("陈昌的！");
+	}
+
+}
---
+++ src/com/tianmao/master/Master.java
@@ -0,0 +1,9 @@
+package com.tianmao.master;
+
+public class Master {
+
+	public static void main(String[] args) {
+		System.out.println("假装这里有很多包，这是主线父类不要动！");
+	}
+
+}
